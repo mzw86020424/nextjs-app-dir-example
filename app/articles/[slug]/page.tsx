6c1ca0433586bd325c64244a2e74a81b37c52763
mzw86020424 <mzw86020424@gmail.com>
2024-03-20 18:04:06 +0900

    記事詳細ページ・notfoundページを実装

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -1,8 +1,35 @@
-export default function Article({ params }: { params: { slug: string } }) {
-  return (
-    <div>
-      <h1>記事の詳細</h1>
-      <p>記事のスラッグ: {params.slug}</p>
-    </div>
+import { notFound } from "next/navigation";
+import { Article, Comment } from "../../types";
+
+const getArticle = async (slug: string) => {
+  const res = await fetch(`http://localhost:3000/api/articles/${slug}`, {
+    next: { revalidate: 60 },
+  });
+
+  if (res.status == 404) {
+    notFound(); // not-found.tsxが表示される
+  }
+
+  if(!res.ok) {
+    throw new Error("Failed to fetch article");
+  }
+
+  const data = await res.json();
+  return data as Article;
+};
+
+const getComments = async (slug: string) => {
+  const res = await fetch(
+    `http://localhost:3000/api/articles/${slug}/comments`,
+    {
+      cache: "no-store",
+    }
   );
+
+  if(!res.ok) {
+    throw new Error("Failed to fetch comments");
+  }
+
+  const data = await res.json();
+  return data as Comment[];
 }
