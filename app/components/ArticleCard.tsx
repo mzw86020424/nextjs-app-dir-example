d0274819c0cfbe21e51a65531c7297a08d578a95
mzw86020424 <mzw86020424@gmail.com>
2024-03-20 17:48:54 +0900

    記事一覧のコンポーネントを作成

--- app/components/ArticleCard.tsx
+++ app/components/ArticleCard.tsx
@@ -5,9 +5,9 @@ import {
   CardFooter,
   Heading,
   Text,
-} from "./common/components";
+} from "../common/components";
 import NextLink from"next/link"
-import { Article } from "./types";
+import { Article } from "../types";
 
 export default function ArticleCard({ article }: { article: Article }) {
   const formattedDate = new Date(article.createdAt).toLocaleDateString(
